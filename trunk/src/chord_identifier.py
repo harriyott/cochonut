bb8e236937f5cd45c0d62dbfdfde2f11f02f396b
esbenbugge <esbenbugge@e809dbd0-d9d9-11de-9872-ef835a306e00>
2009-11-25 16:08:06 +0000

    First commit

---
+++ trunk/src/chord_identifier.py
@@ -0,0 +1,45 @@
+'''
+Created on Nov 22, 2009
+
+@author: epb
+'''
+
+"""
+pitch classes:
+0: C
+1: C#, Db
+2: D
+3: D#, Eb
+4: E
+5: F
+6: F#, Gb
+7: G
+8: G#, Ab
+9: A
+10: A#, Bb
+11: B
+"""
+
+"""
+templates:
+
+The digits in 'Templates' correspond to pitch classes 
+
+Number of notes        Name                    Rating        Template
+---------------------------------------------------------------------
+0                      rest                    15            <>
+1                      single note             14            <0>
+2                      major 3rd / minor 6th   13            <0 4>
+                       minor 3rd / major 6th   12            <0 3>
+                       tritone                 11            <0 6>
+...
+...
+...
+3                      major triad              9            <0 4 7>
+                       minor triad              8            <0 3 7>
+                       diminished triad         7            <0 3 6>
+...
+...
+
+"""
+
